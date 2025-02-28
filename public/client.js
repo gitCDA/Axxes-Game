bd0b568d98fae399fe0ae245b1c3eb73f1085abe
gitCDA <githubCDA@mail.com>
2025-02-28 09:32:47 -0400

    deploy cl

--- public/client.js
+++ public/client.js
@@ -1,19 +1,19 @@
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
-// console.log('first')
-// // Chargement de la configuration depuis .env
-// const firebaseConfig = {
-//   apiKey: process.env.VITE_FIREBASE_API_KEY,
-//   authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.VITE_FIREBASE_APP_ID
-// };
-// console.log(firebaseConfig)
-// console.log(firebaseApp)
-// console.log(db)
-// // import.meta
-// // Initialisation Firebase
-// const firebaseApp = initializeApp(firebaseConfig);
-// const db = getFirestore(firebaseApp);
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+console.log('first')
+// Chargement de la configuration depuis .env
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID
+};
+console.log(firebaseConfig)
+console.log(firebaseApp)
+console.log(db)
+// import.meta
+// Initialisation Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
