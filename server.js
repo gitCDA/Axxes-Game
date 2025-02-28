bd9186f92bbf26a765b51bbab8ff93fe704e49b1
gitCDA <githubCDA@mail.com>
2025-02-28 14:54:07 -0400

    push

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
+// Route pour la page d'accueil
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Démarrer le serveur
 app.listen(port, () => {
   console.log(`Serveur en écoute sur le port ${port}`);
