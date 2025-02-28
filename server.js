08e508a833bcc3035f89e362b850cf1ab7c8616d
gitCDA <githubCDA@mail.com>
2025-02-27 22:46:54 -0400

    netlify3

--- server.js
+++ server.js
@@ -2,25 +2,16 @@ const express = require('express');
 const path = require('path');
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp } = require('firebase-admin/firestore');
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Important pour les sauts de ligne
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-  universe_domain: process.env.UNIVERSE_DOMAIN
-};
+require('dotenv').config();
 
 // Initialisation de Firebase
 initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
 });
-// const serviceAccount = require('./scratch-axxes-firebase-adminsdk-fbsvc-21ad0f39dc.json');
 
 
 const db = getFirestore(); // Initialisation de Firestore
